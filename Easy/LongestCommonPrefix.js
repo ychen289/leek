d918cc4faec5c1ebfb4cd91aa49aa134a97c0ffc
YiFan Chen <ychen289@buffalo.edu>
2023-03-08 23:46:32 -0500

    adding questions

---
+++ Easy/LongestCommonPrefix.js
@@ -0,0 +1,8 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings. 
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Examples: 
+ * input: strs = ["flower", "flow", "flight"]
+ * output: "fl"
+ */

38af82200e58eb6314c127b9fb7cac62a633e814
YiFan Chen <ychen289@buffalo.edu>
2023-03-09 01:09:39 -0500

    added description and solution

--- Easy/LongestCommonPrefix.js
+++ Easy/LongestCommonPrefix.js
@@ -6,3 +6,38 @@
  * input: strs = ["flower", "flow", "flight"]
  * output: "fl"
  */
+
+const strs = ["flower", "flow", "flight"]
+
+var longestCommonPrefix = function(strings) {
+    let commonPrefix = ""
+    // base case if strings is not available, bad, etc
+    if (strings.length === 0 || strings === null) {
+        return commonPrefix
+    }
+    //establish initial minimum length
+    let minimumLength = strings[0].length
+    for (let i=0; i<strings.length; i++) {
+        //Loops so Math.min can repeatedly make minimumLength
+        // the shortest word
+        minimumLength = Math.min(minimumLength, strings[i].length)
+    }
+    //loop through but minimumLength is constraint bc 
+    //mostCommonPrefix/common can only be as large as
+    //the minimumLength
+    for (let i = 0; i<minimumLength; i++) {
+        //Let current be first letter of first word
+        let current = strings[0][i] 
+        //iterates through each word to match first or i letter
+        for (let j=0; j<strings.length;j++) {
+            //if it doesn't match then end everything and just
+            //return commonPrefix as is
+            if (strings[j][i] !== current) {
+                return commonPrefix
+            }
+        }
+        //if successful then add that letter to current
+        commonPrefix += current
+    }
+    return commonPrefix
+};
